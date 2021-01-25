bc02a083a0247d34135c3eba741db971e2db79cf
Park Chanwoo <dev.halo1005@gmail.com>
2021-01-26 00:19:30 +0900

    EnemyFactory

---
+++ Assets/Scripts/EnemyFactory.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFactory : MonoBehaviour
+{
+    public const string EnemyPath = "Prefabs/Enemy";
+
+    Dictionary<string, GameObject> EnemyFileCache = new Dictionary<string, GameObject>();
+
+    public GameObject Load(string resourcePath)
+    {
+        GameObject go;
+        if (EnemyFileCache.ContainsKey(resourcePath))
+        {
+            go = EnemyFileCache[resourcePath];
+        }
+        else
+        {
+            go = Resources.Load<GameObject>(resourcePath);
+            if (!go)
+            {
+                Debug.LogError($"Load error! path = {resourcePath}");
+                return null;
+            }
+
+            EnemyFileCache.Add(resourcePath, go);
+        }
+
+        GameObject InstanceGO = Instantiate(go);
+
+        return InstanceGO;
+    }
+}
